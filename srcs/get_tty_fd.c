67fd71f986373ef3beeea6767ea9014a2d8f8ac1
Aurelien CAZUC <acazuc@student.42.fr>
2016-09-22 10:35:27 +0200

    Fixed tty on backquote

--- srcs/get_tty_fd.c
+++ srcs/get_tty_fd.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_tty_fd.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/21 16:31:50 by acazuc            #+#    #+#             */
+/*   Updated: 2016/09/22 10:33:54 by acazuc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_select.h"
 
 void	get_tty_fd(t_env *env)
@@ -7,9 +19,9 @@ void	get_tty_fd(t_env *env)
 	if (isatty(1))
 	{
 		env->fd = 1;
-		return;
+		return ;
 	}
-	if (!(tmp = ttyname(ttyslot())))
+	if (!(tmp = ttyname(0)))
 	{
 		ft_putendl_fd("ft_select: can't get tty name", 2);
 		exit(EXIT_FAILURE);
